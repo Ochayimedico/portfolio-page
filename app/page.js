cadafb3ea233b484244c18712ccc2a22e36a98c0
unknown <ochayipeter42@gmail.com>
2025-11-30 04:20:31 +0100

    second commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex  w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start text-[2rem]">
-        <a className="mb-4" href="http://ochayimedico.xyz/airdrops-app">
+        <a className="mb-4" href="http://airdrops.ochayimedico.xyz">
           Airdrops App
         </a>
         <a className="mt-1" href="http://ochayimedico.xyz/todo-and-task-app">
