608de0b83e731207abffd09faf98e3f0a3435333
unknown <ochayipeter42@gmail.com>
2025-12-19 01:11:39 +0100

    latest

--- app/page.js
+++ app/page.js
@@ -73,7 +73,7 @@ export default function Home() {
         </motion.nav>
       )}
       {/* hero section */}
-      <section className="flex w-full flex-col items-center justify-center text-[2rem] mt-35">
+      <section className="flex w-full h-dvh flex-col items-center justify-center text-[2rem] m-2">
         <p className="absolute uppercase -rotate-90 -left-25 text-sm top-100 tracking-[0.3rem]">
           ochayi peter ago
         </p>
@@ -87,7 +87,7 @@ export default function Home() {
           <h3 className="text-center md:text-[2.5rem] text-[2rem] lg:text-[3rem] text-[#1B1508] ">
             Website Designer
           </h3>
-          <p className="text-sm text-center lg:mt-34 mt-40 mb-25 text-[#9C7630]">
+          <p className="text-sm text-center justify-end lg:mt-34 mt-40  text-[#9C7630]">
             scroll down pls
           </p>
         </div>
@@ -239,14 +239,13 @@ export default function Home() {
           </motion.p>
         </motion.div>
       </motion.section>
-      {/* picture */}
+      {/* picture section*/}
       <motion.section
         initial="hidden"
         whileInView="visible"
         variants={{
-          hidden: { borderRadius: "0" },
+          hidden: {},
           visible: {
-            borderRadius: "60%/ 0 0 10% 10%",
             transition: {
               duration: 0.5,
               staggerChildren: 0.8,
@@ -254,7 +253,7 @@ export default function Home() {
             },
           },
         }}
-        className="bg-[#E7D4B1] w-full p-8 "
+        className="bg-[#E7D4B1] w-full p-8  h-max"
       >
         <motion.p
           variants={{
@@ -279,8 +278,10 @@ export default function Home() {
 
         <motion.div
           variants={{
-            hidden: { opacity: 0, scale: 0 },
+            hidden: { opacity: 0, scale: 0, borderRadius: "60%/ 0 0 5% 5%" },
             visible: {
+              borderRadius: "0%",
+
               opacity: 1,
               scale: 1,
               transition: {
@@ -334,14 +335,172 @@ export default function Home() {
               },
             },
           }}
-          className="mt-4 mb-4 text-black md:text-[2rem] lg:text-[2.5rem] text-[1.5rem]"
+          className="px-1 mt-4 mb-4 text-black md:text-[2rem] lg:text-[2.5rem] text-[1.5rem]"
         >
           Ochayi is a pharmacist and graphics designer who fell in love with
           website design and frontend development.
           <br />
-          The ultimate goal is user satisfaction after when using an app created
-          by me.
+          For me, the ultimate goal is user satisfaction after using an app
+          created by me.
         </motion.p>
+
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, y: 30 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: {
+                duration: 2,
+                delay: 1.95,
+                type: "spring",
+                stiffness: 100,
+                damping: 12,
+                ease: "easeInOut",
+              },
+            },
+          }}
+          className="flex lg:flex-row md:flex-row flex-col justify-between my-8"
+        >
+          <div className="lg:w-[40%] md:w-[40%] mb-2 w-full flex flex-col text-black">
+            <div className=" h-0.5 bg-[#3f2728] w-full mb-4"> </div>
+            <p className="uppercase text-[0.85rem]">years of experience</p>
+            <p className="uppercase font-bold text-[4rem]">2+</p>
+          </div>
+          <div className="lg:w-[40%] md:w-[40%]  w-full flex flex-col text-black">
+            <div className=" h-0.5 bg-[#3f2728] w-full mb-4"> </div>
+            <p className="uppercase text-[0.85rem]">
+              number of projects completed
+            </p>
+            <p className="uppercase font-bold text-[4rem]">over 10</p>
+          </div>
+        </motion.div>
+      </motion.section>
+
+      {/* my projects */}
+      <motion.section
+        initial={{}}
+        whileInView={{}}
+        transition={{
+          delay: 0.1,
+          duration: 1.5,
+          type: "spring",
+          stiffness: 80,
+          damping: 10,
+          bounce: 0.6,
+          ease: "easeInOut",
+        }}
+        className="bg-[#3f2728] w-full relative"
+      >
+        <motion.div
+          className="mt-2 p-8 text-white text-center md:text-[1.85rem] lg:text-[2.25rem] text-[1.35rem] w-[90%] md:w-[70%] lg:w-[60%] mx-auto"
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: {},
+            visible: {
+              transition: {
+                staggerChildren: 0.8,
+                delayChildren: 1,
+              },
+            },
+          }}
+        >
+          <motion.div
+            className="w-max relative mx-auto"
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: {},
+              visible: {
+                transition: {
+                  staggerChildren: 0.8,
+                  delayChildren: 0,
+                },
+              },
+            }}
+          >
+            <motion.h2
+              variants={{
+                hidden: { opacity: 0, y: 30 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.3,
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 12,
+                    ease: "easeInOut",
+                  },
+                },
+              }}
+              className="text-[#ffffff] text-center md:text-[2rem] lg:text-[2.5rem] text-[1.5rem] my-6 transition-all w-max mx-auto"
+            >
+              My Projects
+            </motion.h2>
+
+            {/* line */}
+            <motion.div
+              className="bg-[#ffffff] absolute lg:bottom-2 bottom-0 md:bottom-1 h-1"
+              variants={{
+                hidden: { opacity: 0, width: "0%" },
+                visible: {
+                  width: "100%",
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.8,
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 12,
+                    ease: "easeInOut",
+                  },
+                },
+              }}
+            />
+          </motion.div>
+          <motion.p
+            variants={{
+              hidden: { opacity: 0, y: 30 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.3,
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 12,
+                  ease: "easeInOut",
+                },
+              },
+            }}
+          >
+            These are the top projects I have worked on in the past months
+          </motion.p>
+
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 30 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.3,
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 12,
+                  ease: "easeInOut",
+                },
+              },
+            }}
+            className="mt-8 text-left"
+          >
+            <motion.h2 className="text-[2.5rem]  font-bold text-[#e7d4b1]">
+              Airdrops App
+            </motion.h2>
+          </motion.div>
+        </motion.div>
       </motion.section>
     </div>
   );
